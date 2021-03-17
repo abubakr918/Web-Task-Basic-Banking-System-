d2afcb21688df85c168b1f490840e850098bf552
Muhammad Abubakr <56479069+abubakr918@users.noreply.github.com>
2021-03-17 20:55:51 +0500

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,27 @@
+
+
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-orange" >
+      <a class="navbar-brand" href="index.php" style="color :orange;"><b>STANDARD CHARTERED BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+
+              <li class="nav-item">
+                <a class="nav-link" href="users.php" style="color : orange;"><b>Users</b></a>
+              </li>
+             
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color :orange;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : orange;"><b>Transactions</b></a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : orange;"><b>Home</b></a>
+              </li>
+          </div>
+       </nav>
